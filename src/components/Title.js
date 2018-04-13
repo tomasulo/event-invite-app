e11e20b625a776e3f3168ab2ed346b3b400c0922
Thomas Kieffer <thomas.kieffer@codecentric.de>
2018-04-13 14:18:29 +0200

    Replace user with owner

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Header, Icon} from "semantic-ui-react";
 
-export const Title = ({title, user}) => (
+export const Title = ({title, owner}) => (
     <Header as='h1' icon dividing textAlign='center'>
         <Icon name='users' circular/>
         {title}
         <Header.Subheader>
-            Created by {user}
+            Created by {owner}
         </Header.Subheader>
     </Header>
 );
